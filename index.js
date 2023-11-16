dc265a9c13ff8a4ffa5e81e438468952a6f4f5aa
Jonathan Hurd <hurd.jonathan@gmail.com>
2023-11-16 11:05:32 -0600

    Added route for fetching single resource

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-let phonebook = [
+let persons = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -27,7 +27,18 @@ let phonebook = [
 ]
 
 app.get('/api/persons', (request, response) => {
-  response.json(phonebook);
+  response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+   const id = Number(request.params.id)
+   const person = persons.find(person => person.id === id);
+
+   if (person) {
+    response.json(person);
+   } else {
+    response.status(404).end();
+   }
 });
 
 app.get('/info', (request, response) => {
@@ -41,9 +52,11 @@ app.get('/info', (request, response) => {
     second: 'numeric',
     timeZoneName: 'long'
   }
+  const date = new Date().toLocaleDateString("en-US", dateOptions);
+
   const responseText = 
-  `<p>Phonebook has info for ${phonebook.length} people.</p>
-  ${new Date().toLocaleDateString("en-US", dateOptions)}`  
+  `<p>Phonebook has info for ${persons.length} people.</p>
+  ${date}`  
 
   response.end(responseText);
 });
